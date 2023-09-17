01c8138d15935b6ee0b04ebd88b76ea588b82414
Tobias Olea <tobiolea97@gmail.com>
2023-09-17 11:57:03 -0300

    1. set up of java project for learning jdbc

---
+++ java-jdbc/src/main/java/org/mule/java/jdbc/JDBCExecutor.java
@@ -0,0 +1,14 @@
+package org.mule.java.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class JDBCExecutor
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
