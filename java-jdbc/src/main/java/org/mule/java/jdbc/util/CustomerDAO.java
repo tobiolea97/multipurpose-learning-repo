359c933113b2a264a2a5ea407c7e4bdbf8c332f6
Tobias Olea <tobiolea97@gmail.com>
2023-09-17 18:05:03 -0300

    Commits and Rollback

--- java-jdbc/src/main/java/org/mule/java/jdbc/util/CustomerDAO.java
+++ java-jdbc/src/main/java/org/mule/java/jdbc/util/CustomerDAO.java
@@ -63,6 +63,14 @@ public class CustomerDAO extends DataAccessObject<Customer> {
     @Override
     public Customer update(Customer dto) {
         Customer customer = null;
+
+        try{
+            this.connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
         try(PreparedStatement statement = this.connection.prepareStatement(UPDATE);){
             statement.setString(1, dto.getFirstName());
             statement.setString(2, dto.getLastName());
@@ -74,8 +82,15 @@ public class CustomerDAO extends DataAccessObject<Customer> {
             statement.setString(8, dto.getZipCode());
             statement.setLong(9, dto.getId());
             statement.execute();
+            this.connection.commit();
             customer = this.findById(dto.getId());
         }catch(SQLException e){
+            try{
+                this.connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex);
+            }
             e.printStackTrace();
             throw new RuntimeException(e);
         }
