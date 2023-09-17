bf1a75aea0a6b40857c39f837a29f3e2f0deab2d
Tobias Olea <tobiolea97@gmail.com>
2023-09-17 16:36:04 -0300

    Deleting data

--- java-jdbc/src/main/java/org/mule/java/jdbc/util/CustomerDAO.java
+++ java-jdbc/src/main/java/org/mule/java/jdbc/util/CustomerDAO.java
@@ -17,6 +17,8 @@ public class CustomerDAO extends DataAccessObject<Customer> {
     private static final String UPDATE = "UPDATE customer SET first_name = ?, last_name=?, " +
             "email = ?, phone = ?, address = ?, city = ?, state = ?, zipcode = ? WHERE customer_id = ?";
 
+    private static final String DELETE = "DELETE FROM customer WHERE customer_id = ?";
+
     public CustomerDAO(Connection connection) {
         super(connection);
     }
@@ -98,6 +100,12 @@ public class CustomerDAO extends DataAccessObject<Customer> {
 
     @Override
     public void delete(long id) {
-
+        try(PreparedStatement statement = this.connection.prepareStatement(DELETE);){
+            statement.setLong(1, id);
+            statement.execute();
+        }catch (SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
