219ab449e6440079b78aa5bc306ad1e556564c39
Tobias Olea <tobiolea97@gmail.com>
2023-09-18 16:11:39 -0300

    Implementation of After advice

--- java-spring/src/main/java/com/bbva/forum/aspect/LoggingAspect.java
+++ java-spring/src/main/java/com/bbva/forum/aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.bbva.forum.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 @Component
 @Aspect
@@ -19,8 +21,8 @@ public class LoggingAspect {
     @Pointcut("@annotation(Loggable)")
     public void executeLogging() {}
 
-    @Before("executeLogging()")
-    public void logMethodCall(JoinPoint joinPoint) {
+    @AfterReturning(value = "executeLogging()", returning = "returnValue")
+    public void logMethodCall(JoinPoint joinPoint, Object returnValue) {
         StringBuilder message = new StringBuilder("Mehtod: ");
         message.append(joinPoint.getSignature().getName());
         Object[] args = joinPoint.getArgs();
@@ -30,6 +32,14 @@ public class LoggingAspect {
                 message.append(arg).append(" | ");
             });
             message.append(("]"));
+        }
+        if(returnValue instanceof Collection) {
+            message.append(", returning : ")
+                    .append(((Collection)returnValue).size())
+                    .append(" instance(s)");
+        } else {
+            message.append(", returning ")
+                    .append(returnValue.toString());
         }
         LOGGER.info(message.toString());
     }
