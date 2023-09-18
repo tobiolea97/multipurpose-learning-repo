92e5c0fa4eac2e7b7065f64f2813e4848a34ea93
Tobias Olea <tobiolea97@gmail.com>
2023-09-17 23:04:24 -0300

    Accessing collection elements with iterator

--- java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
+++ java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
@@ -1,5 +1,10 @@
 package com.mule.java.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
+        Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+        Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
+
+        Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly, cambridge, westminister));
+
+        Iterator<Room> iterator = rooms.iterator();
+        System.out.println(iterator.next().getName());
+
+        //prior to Java 5
+        while(iterator.hasNext()) {
+            Room room = iterator.next();
+            System.out.println(room.getName());
+        }
+
+        // newer approach
+        Iterator<Room> iterator2 = rooms.iterator();
+        for (Room room : rooms ) {
+            System.out.println(room.getName());
+        }
+
     }
 }
