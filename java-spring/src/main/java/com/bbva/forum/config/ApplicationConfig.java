ad0087dfe20ff076d2a4fc43c6913c18dee3f436
Tobias Olea <tobiolea97@gmail.com>
2023-09-18 16:10:19 -0300

    Aspect oriented programming (AoP). Use of Aspects and pointcuts.

--- java-spring/src/main/java/com/bbva/forum/config/ApplicationConfig.java
+++ java-spring/src/main/java/com/bbva/forum/config/ApplicationConfig.java
@@ -2,12 +2,15 @@ package com.bbva.forum.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.bbva.forum")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
+    
 
 }
