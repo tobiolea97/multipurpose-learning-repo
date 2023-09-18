edea6bb2b8a9568d45186cbd588aa25f18c027c8
Tobias Olea <tobiolea97@gmail.com>
2023-09-18 16:08:48 -0300

    Bean Factory setup based on Component Scanning

--- java-spring/src/main/java/com/bbva/forum/service/OutputService.java
+++ java-spring/src/main/java/com/bbva/forum/service/OutputService.java
@@ -1,16 +1,22 @@
 package com.bbva.forum.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OutputService {
 
-    private final String name;
+    @Value("${app.name}")
+    private String name;
 
     private final GreetingService greetingService;
     private final TimeService timeService;
 
-    public OutputService(GreetingService greetingService, TimeService timeService, String name){
+    @Autowired //this is not necessary as it is the only constructor we have
+    public OutputService(GreetingService greetingService, TimeService timeService){
         this.greetingService = greetingService;
         this.timeService = timeService;
-        this.name = name;
     }
 
     public void generateOutput(){
