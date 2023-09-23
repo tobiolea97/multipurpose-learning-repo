2635cfae35b31437596db3c4cf30f3d5832190cd
Tobias Olea <tobiolea97@gmail.com>
2023-09-23 15:24:17 -0300

    Finding elements in a collections

--- java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
+++ java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
@@ -17,17 +17,25 @@ public class App {
         Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
         Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
         Room oxford = new Room("Oxford", "Suite", 5, 225.0);
-        Room oxfordGuest = new Room("Oxford", "Guest Room", 5, 225.0);
+        Room oxford1 = new Room("Oxford", "Guest Room", 5, 225.0);
         Room victoria = new Room("Victoria", "Suite", 5, 225.0);
 
-        List<Room> rooms = new ArrayList<>(List.of(piccadilly, oxfordGuest, cambridge, victoria, oxford));
+        List<Room> rooms = new ArrayList<>(List.of(piccadilly, oxford1, cambridge, victoria, oxford));
+        rooms.sort(Room.RATE_COMPARATOR);
 
-        rooms.sort(Room.RATE_COMPARATOR.reversed());
+        Room newLondon = new Room("New London", "Suite", 5, 225.0);
+        int result = Collections.binarySearch(rooms, newLondon, Room.RATE_COMPARATOR);
+        rooms.add(Math.abs(++result), newLondon);
+        System.out.println("Result: " + result);
 
+        System.out.println("Min: " + Collections.min(rooms, Room.RATE_COMPARATOR).getName());
+        System.out.println("Max: " + Collections.max(rooms, Room.RATE_COMPARATOR).getName());
+
+
+
+        System.out.format("%n%n--List Contents--%n");
         rooms.stream()
                 .forEach(r -> System.out.format("%-15s %-15s %-10f %n", r.getName(), r.getType(), r.getRate()));
-
-
     }
 
 }
