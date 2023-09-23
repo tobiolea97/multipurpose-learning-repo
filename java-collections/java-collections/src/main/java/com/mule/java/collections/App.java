f7d74b4d44dac06f73f101d4aedd58194c6f2b10
Tobias Olea <tobiolea97@gmail.com>
2023-09-23 00:21:04 -0300

    Use of Sets and HashSets

--- java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
+++ java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
@@ -1,9 +1,6 @@
 package com.mule.java.collections;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -16,49 +13,29 @@ public class App
 {
     public static void main(String[] args) {
 
-        Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
-        Room manchester = new Room("Manchester", "Suite", 5, 250.00);
+        Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
         Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+        Room oxfordDuplicate = new Room("Oxford", "Suite", 5, 225.0);
         Room victoria = new Room("Victoria", "Suite", 5, 225.00);
 
-        Collection<Room> rooms = new ArrayList<>(Arrays.asList(cambridge, oxford, victoria, manchester));
-        oxford.setPetFriendly(true);
-        victoria.setPetFriendly(true);
+        Set<Room> otherRooms = Set.of(piccadilly,oxford);
+        //otherRooms.add(victoria); // this breaks the code because Set.of() creates an unchangable object
+
+        //Set<Room> rooms = new LinkedHashSet<>(); // LinkedHashSet keeps the order of insertion
+        Set<Room> rooms = new HashSet<>(); // HashSet doesn't keep the order of insertion
+        rooms.add(piccadilly);
+        rooms.add(oxford);
+        rooms.add(oxford);
+        rooms.add(oxfordDuplicate);
 
 
+        Set<Room> moreRooms = Set.copyOf(rooms);
 
-        // ****** NEVER WORK WITH EXTERNAL OBJECTS INSIDE YOUR STREAM AND
-        // ****** MODIFY OBJECTS INSIDE YOUR STREAM AT THE SAME TIME
-        /*
-        Collection<Room> petFriendlyRooms = new ArrayList<>();
-        rooms.stream()
-                .filter(Room::isPetFriendly)
-                .forEach(room -> petFriendlyRooms.add(room));
-        */
-
-        // THIS IS THE CORRECT WAY
-        Collection<Room> petFriendlyRooms = rooms.stream()
-                .filter(Room::isPetFriendly)
-                .collect(Collectors.toList());
-
-        // THIS IS ONE ALTERNATIVE FOR PRINTING OUT
-        petFriendlyRooms.stream()
-                .forEach(r -> System.out.println(r.getName()));
-
-        // THIS IS A BETTER ALTERNATIVE FOR PRINTING OUT
-        petFriendlyRooms.stream()
-                // map transform the element that flows out of the operation
-                // after this, the stream will be only a list of names
+        moreRooms.stream()
                 .map(r -> r.getName())
-                // System.out::println is a method reference
                 .forEach(System.out::println);
 
 
-        double total = petFriendlyRooms.stream()
-                .mapToDouble(Room::getRate)
-                .sum();
-
-        System.out.println(total);
 
     }
 }
