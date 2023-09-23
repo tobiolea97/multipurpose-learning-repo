bf8146db7557a0f76cf66cb53d4790b8751b3089
Tobias Olea <tobiolea97@gmail.com>
2023-09-23 14:47:45 -0300

    Iterating maps

--- java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
+++ java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
@@ -1,5 +1,6 @@
 package com.mule.java.collections;
 
+import java.security.KeyStore;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -13,23 +14,25 @@ public class App {
 
     public static void main(String[] args) {
 
-        //RoomWithBadKey piccadilly = new RoomWithBadKey("Piccadilly", "Guest Room", 3, 125.00);
-        //RoomWithBadKey oxford = new RoomWithBadKey("Oxford", "Suite", 5, 225.0);
         Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
         Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+
         Guest john = new Guest("John", "Doe", false);
         Guest maria = new Guest("Maria", "Doe", true);
 
-        //Map<RoomWithBadKey,Guest> assignments = new HashMap<>();
-        Map<Room,Guest> assignments = new HashMap<>();
-        assignments.put(oxford,maria);
-        assignments.put(piccadilly,john);
+        Map<Room, Guest> assignments = new HashMap<>();
+        assignments.put(oxford, maria);
+        assignments.put(piccadilly, john);
 
-        Guest guest = assignments.put(piccadilly, assignments.remove(oxford));
-        assignments.putIfAbsent(oxford, guest);
+        Set<Map.Entry<Room, Guest>> collectionView = assignments.entrySet();
 
-        System.out.println("Oxford: " + assignments.get(new Room("Oxford","Suite",5,225.0)));
-        System.out.println("Picadilly: " + assignments.get(piccadilly));
+        for (Map.Entry<Room,Guest> entry:
+             assignments.entrySet()) {
+            Room r = entry.getKey();
+            Guest g = entry.getValue();
+            System.out.format("%s : %s%n",r.getName(), g.getFirstName());
+        }
+
 
 
     }
