7bee62b8339ba7c60964a65310b7fe061e4b117f
Tobias Olea <tobiolea97@gmail.com>
2023-09-23 09:56:31 -0300

    Usage of ArrayList

--- java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
+++ java-collections/java-collections/src/main/java/com/mule/java/collections/App.java
@@ -13,17 +13,36 @@ public class App
 {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
+        Guest john = new Guest("John", "Doe", false);
+        Guest maria = new Guest("Maria", "Doe", false);
+        Guest sonia = new Guest("Sonia", "Doe", true);
+        Guest siri = new Guest("Siri", "Doe", true);
 
-        NavigableSet<Integer> numberTree = new TreeSet<>(numbers);
+        List<Guest> checkinList = new ArrayList<>(100); // 100 inicial capacity
+        checkinList.add(john);
+        checkinList.add(maria);
+        print(checkinList);
 
-        numberTree
-                //.descendingSet()
-                .headSet(1750)
-                .tailSet(750)
-                .subSet(1499,1501)
-                .stream()
-                .forEach(System.out::println);
+        checkinList.add(0,siri);
+        checkinList.get(2).setLoyaltyProgramMember(true);
+
+        checkinList.addAll(1, List.of(maria, siri));  // insert the list of the second parameter in the index of the first parameter
+                                                            // following elements are moved to the right
+
+        checkinList.remove(checkinList.size() - 1);
+
+        System.out.println("John is: " + checkinList.indexOf(john));
+
+    }
+
+    public static void print(List<Guest> list) {
+
+        System.out.format("%n--List Contents--%n");
+
+        for (int x = 0; x < list.size(); x++) {
+            Guest guest = list.get(x);
+            System.out.format("%x: %s %n", x, guest.toString());
+        }
 
     }
 }
