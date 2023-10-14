74834fc175543d7a2ccdece887783f46ca1c6532
Tobias Olea <tobiolea97@gmail.com>
2023-10-14 10:03:17 -0300

    Building a custom hook

--- react-essential-training/react-app/src/App.js
+++ react-essential-training/react-app/src/App.js
@@ -1,32 +1,38 @@
 import "./App.css";
 import { useState } from "react";
 
+function useInput(initialValue) {
+  const [value, setValue] =
+    useState(initialValue);
+  return [
+    {
+      value,
+      onChange: (e) => setValue(e.target.value)
+    },
+    () => setValue(initialValue)
+  ];
+}
+
 function App() {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] =
+    useState("#000000");
   const submit = (e) => {
     e.preventDefault();
-    alert(`${title}, ${color}`);
-    setTitle("");
-    setColor("#000000");
+    alert(
+      `${titleProps.value}, ${colorProps.value}`
+    );
+    resetTitle();
+    resetColor();
   };
   return (
     <form onSubmit={submit}>
       <input
-        value={title}
-        onChange={(event) =>
-          setTitle(event.target.value)
-        }
+        {...titleProps}
         type="text"
         placeholder="color title..."
       />
-      <input
-        value={color}
-        type="color"
-        onChange={(event) =>
-          setColor(event.target.value)
-        }
-      />
+      <input {...colorProps} type="color" />
       <button>ADD</button>
     </form>
   );
