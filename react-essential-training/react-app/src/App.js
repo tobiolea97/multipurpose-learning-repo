7d15aaa4cd4f4dda2fc94ca3a6eb3ef2488bfd99
Tobias Olea <tobiolea97@gmail.com>
2023-10-14 09:07:17 -0300

    useState hook

--- react-essential-training/react-app/src/App.js
+++ react-essential-training/react-app/src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
+import { useState } from "react";
 
-const [firstCity, second] = [
-  "Tokyo",
-  "Tahoe City",
-  "Bend"
-];
+function App() {
+  const [emotion, setEmotion] = useState();
 
-console.log(firstCity);
-console.log(second);
-
-function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>
+        Sad
+      </button>
+      <button
+        onClick={() => setEmotion("excited")}
+      >
+        Excited
+      </button>
     </div>
   );
 }
