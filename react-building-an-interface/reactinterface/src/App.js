72813f0b30efd7cebcd99d0babea7156fa9aa217
Tobias Olea <tobiolea97@gmail.com>
2023-10-14 19:13:11 -0300

    Searching with a filtered array

--- react-building-an-interface/reactinterface/src/App.js
+++ react-building-an-interface/reactinterface/src/App.js
@@ -6,7 +6,16 @@ import AppointmentInfo from "./components/AppointmentInfo"
 
 function App() {
 
-let[appointmentList, setAppointmentList] = useState([]);
+  let[appointmentList, setAppointmentList] = useState([]);
+  let[query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return  item.petName.toLowerCase().includes(query.toLowerCase()) ||
+              item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+              item.aptNotes.toLowerCase().includes(query.toLowerCase())
+    }
+  )
 
   const fetchData = useCallback(()=> {
     fetch('./data.json')
@@ -25,11 +34,12 @@ let[appointmentList, setAppointmentList] = useState([]);
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
         <AddAppointment />
-        <Search />
+        <Search query={query} onQueryChange={myQuery => setQuery(myQuery)} />
 
         <ul className="divide-y divide-gray-200">
           {
-            appointmentList.map(appointment => (
+            filteredAppointments
+              .map(appointment => (
                 <AppointmentInfo key={appointment.id}
                   appointment={appointment}
                   onDeleteAppointment={
