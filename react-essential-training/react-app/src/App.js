4930a1a6a0059265b8e6966098f87066da7779e4
Tobias Olea <tobiolea97@gmail.com>
2023-10-14 09:51:07 -0300

    useRef

--- react-essential-training/react-app/src/App.js
+++ react-essential-training/react-app/src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useRef } from "react";
 
 function App() {
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  const txtTitle = useRef();
+  const hexColor = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`);
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  };
   return (
-    <div className="App">
+    <form onSubmit={submit}>
       <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
+        ref={txtTitle}
+        type="text"
+        placeholder="color title..."
       />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
-    </div>
+      <input ref={hexColor} type="color" />
+      <button>ADD</button>
+    </form>
   );
 }
 
