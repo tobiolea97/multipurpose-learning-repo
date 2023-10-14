8d3035ff41257f22068b596fb093790dcc2a26a0
Tobias Olea <tobiolea97@gmail.com>
2023-10-14 19:31:55 -0300

    Programming sorting functionality

--- react-building-an-interface/reactinterface/src/App.js
+++ react-building-an-interface/reactinterface/src/App.js
@@ -41,7 +41,12 @@ function App() {
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
         <AddAppointment />
-        <Search query={query} onQueryChange={myQuery => setQuery(myQuery)} />
+        <Search query={query}
+          onQueryChange={myQuery => setQuery(myQuery)}
+          orderBy={orderBy}
+          onOrderByChange={mySort => setOrderBy(mySort)}
+          sortBy={sortBy}
+          onSortByChange={mySort => setSortBy(mySort)} />
 
         <ul className="divide-y divide-gray-200">
           {
