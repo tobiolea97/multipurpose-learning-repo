5ae729456aefeab0cff804c7a24c2769e614cfa9
Tobias Olea <tobiolea97@gmail.com>
2023-10-14 09:01:55 -0300

    Destructuring. App({library})

--- react-essential-training/react-app/src/App.js
+++ react-essential-training/react-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const [firstCity, second] = [
+  "Tokyo",
+  "Tahoe City",
+  "Bend"
+];
+
+console.log(firstCity);
+console.log(second);
+
+function App({ library }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
