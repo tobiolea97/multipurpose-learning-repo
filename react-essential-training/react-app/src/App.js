8c994bd8573e83ae6f04ab1e5cdcda376e2fa35c
Tobias Olea <tobiolea97@gmail.com>
2023-10-14 09:15:56 -0300

    More of useEffect

--- react-essential-training/react-app/src/App.js
+++ react-essential-training/react-app/src/App.js
@@ -3,11 +3,15 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
-
+  const [secondary, setSecondary] =
+    useState("tired");
   useEffect(() => {
     console.log(`It's ${emotion} around here!`);
   }, [emotion]);
 
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
@@ -18,6 +22,14 @@ function App() {
         onClick={() => setEmotion("excited")}
       >
         Excited
+      </button>
+      <h2>
+        Current secondary emotion is {secondary}.
+      </h2>
+      <button
+        onClick={() => setSecondary("grateful")}
+      >
+        Grateful
       </button>
     </div>
   );
