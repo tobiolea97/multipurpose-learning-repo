e47b22d2999617bc25a100875f81e9359bef49c0
Tobias Olea <tobiolea97@gmail.com>
2023-10-14 10:16:31 -0300

    Displaying data from API

--- react-essential-training/react-app/src/App.js
+++ react-essential-training/react-app/src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+function GithubUser({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
@@ -12,7 +22,11 @@ function App() {
   }, []);
   if (data)
     return (
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <GithubUser
+        name={data.name}
+        location={data.location}
+        avatar={data.avatar_url}
+      />
     );
   return <h1>Data</h1>;
 }
