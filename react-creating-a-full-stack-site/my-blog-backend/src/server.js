db35c1e1c421c6f415ce9a08282b43d63d956611
Tobias Olea <tobiolea97@gmail.com>
2023-10-15 01:18:15 -0300

    Rewriting upvote endpoint

--- react-creating-a-full-stack-site/my-blog-backend/src/server.js
+++ react-creating-a-full-stack-site/my-blog-backend/src/server.js
@@ -21,9 +21,17 @@ app.get('/api/articles/:name', async (req, res) => {
     }
 });
 
-app.put('/api/articles/:name/upvote', (req, res) => {
+app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-    const article = articlesInfo.find(a => a.name === name);
+   
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+    await db.collection('articles').updateOne({ name }, {
+        $inc: { upvotes: 1 },
+    });
+    const article = await db.collection('articles').findOne({ name });
 
     if (article) {
         article.upvotes += 1;
