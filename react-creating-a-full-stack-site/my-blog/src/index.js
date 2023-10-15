d14ae229888b275f5671533b62a01078e76e8d6e
Tobias Olea <tobiolea97@gmail.com>
2023-10-15 10:46:20 -0300

    Firebase setup. Scaffolding of create account and login pages

--- react-creating-a-full-stack-site/my-blog/src/index.js
+++ react-creating-a-full-stack-site/my-blog/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKJQ89nOvI6CV8R5rWFO7bge6PD954JDA",
+  authDomain: "my-react-blog-2c094.firebaseapp.com",
+  projectId: "my-react-blog-2c094",
+  storageBucket: "my-react-blog-2c094.appspot.com",
+  messagingSenderId: "994835883227",
+  appId: "1:994835883227:web:cde7a3b0dddabc69cc58fe"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
