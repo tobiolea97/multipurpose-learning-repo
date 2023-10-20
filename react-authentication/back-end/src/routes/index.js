0ec9b72e8ab675cbf3a4b66fa3969807ea368b92
Tobias Olea <tobiolea97@gmail.com>
2023-10-20 11:32:25 -0300

    Add login route to index.js

--- react-authentication/back-end/src/routes/index.js
+++ react-authentication/back-end/src/routes/index.js
@@ -1,7 +1,9 @@
 import { signUpRoute } from './signUpRoute';
 import { testRoute } from './testRoute';
+import { logInRoute } from './logInRoute';
 
 export const routes = [
     signUpRoute,
     testRoute,
+    logInRoute,
 ];
