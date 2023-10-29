9993ad86b92987daddbac96f5060218cc6a869fa
Tobias Olea <tobiolea97@gmail.com>
2023-10-29 16:05:54 -0300

    Adding an activity interface

--- react-udemy-course/client-app/src/app/layout/App.tsx
+++ react-udemy-course/client-app/src/app/layout/App.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
+import { Activity } from "../models/activity";
+import { Header } from "semantic-ui-react";
+import List from "semantic-ui-react/dist/commonjs/elements/List";
 
 function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    const activity: Activity = {
+      id: "1",
+      title: "Past Activity 1",
+      date: "2020-01-01",
+      category: "drinks",
+      description: "Activity 1",
+      city: "London",
+      venue: "Pub",
+    };
+    const activities = [activity];
+    setActivities(activities);
+  }, []);
 
   return (
-    <h1>
-      Reactivities
-    </h1>
+    <div>
+      <Header as = 'h2' icon = 'users' content = 'Reactivities' />
+      <List>
+        {activities.map((activity: Activity) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+    </div>
   )
 }
 
