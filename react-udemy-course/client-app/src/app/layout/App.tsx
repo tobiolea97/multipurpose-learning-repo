11bc3314ca01d767d51fa33eb8d0feeed0fe5cd6
Tobias Olea <tobiolea97@gmail.com>
2023-10-30 07:05:17 -0300

    Posting data to the server

--- react-udemy-course/client-app/src/app/layout/App.tsx
+++ react-udemy-course/client-app/src/app/layout/App.tsx
@@ -12,7 +12,7 @@ function App() {
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [loading, setLoading] = useState(true);
   const [editMode, setEditMode] = useState(false);
-
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
     agent.Activities.list()
@@ -49,12 +49,24 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity) {
-    activity.id
-      ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivities([...activities, { ...activity, id: uuid() }]);
-    setEditMode(false);
-    setSelectedActivity(activity);
-  }
+    setSubmitting(true);
+    if (activity.id) {
+        agent.Activities.update(activity).then(() => {
+            setActivities([...activities.filter(x => x.id !== activity.id), activity]);
+            setSelectedActivity(activity);
+            setEditMode(false);
+            setSubmitting(false);
+        })
+    } else {
+        activity.id = uuid();
+        agent.Activities.create(activity).then(() => {
+            setActivities([...activities, activity]);
+            setSelectedActivity(activity);
+            setEditMode(false);
+            setSubmitting(false);
+        })
+    }
+}
 
   if (loading) return <LoadingComponent content='Loading app...' />
 
@@ -72,6 +84,7 @@ function App() {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
     </>
--- react-udemy-course/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ react-udemy-course/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,11 +15,11 @@ interface Props {
     editMode: boolean;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
-
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({ activities, selectedActivity, selectActivity,
-  cancelSelectActivity, openForm, closeForm, editMode, createOrEdit, deleteActivity }: Props) {
+  cancelSelectActivity, openForm, closeForm, editMode, createOrEdit, deleteActivity, submitting }: Props) {
   return (
     <Grid>
         <Grid.Column width='10'>
@@ -36,7 +36,9 @@ export default function ActivityDashboard({ activities, selectedActivity, select
                     <ActivityForm 
                         closeForm={closeForm} 
                         activity={selectedActivity}
-                        createOrEdit={createOrEdit} />
+                        createOrEdit={createOrEdit}
+                        submitting={submitting}
+                        />
             }
         </Grid.Column>
     </Grid>
