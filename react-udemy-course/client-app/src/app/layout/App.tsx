ef48b389ba858b281eda31b1b4de3cf1025cde13
Tobias Olea <tobiolea97@gmail.com>
2023-10-31 19:50:29 -0300

    Refactoring the app to use Mobx

--- react-udemy-course/client-app/src/app/layout/App.tsx
+++ react-udemy-course/client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Activity } from "../models/activity";
-import { Button, Container} from "semantic-ui-react";
+import { Container} from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from 'uuid';
@@ -14,22 +14,12 @@ function App() {
 
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [loading, setLoading] = useState(true);
   const [editMode, setEditMode] = useState(false);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list()
-      .then(response => {
-        let activities: Activity[] = [];
-        response.forEach(activity => {
-          activity.date = activity.date.split('T')[0];
-          activities.push(activity);
-        })
-        setActivities(activities);
-        setLoading(false);
-      })
-  }, [])
+    activityStore.loadActivities();
+  }, [activityStore])
 
   function handleSelectActivity(id: string) {
     setSelectedActivity(activities.find(x => x.id === id));
@@ -76,16 +66,14 @@ function App() {
     }
 }
 
-  if (loading) return <LoadingComponent content='Loading app...' />
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
 
   return (
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: '7em'}} >
-        <h2>{activityStore.title}</h2>
-        <Button content='Add exclamation!' positive onClick={activityStore.setTitle} />
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelect}
