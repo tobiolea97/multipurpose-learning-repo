55abaecded16fc0f60cac7d19c1fd0def20425c5
Tobias Olea <tobiolea97@gmail.com>
2023-11-02 01:01:06 -0300

    Using route parameters

--- react-udemy-course/client-app/src/features/activities/details/ActivityDetails.tsx
+++ react-udemy-course/client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
 import {Button, Card, Image} from "semantic-ui-react";
+import { Link, useParams } from "react-router-dom";
 import { useStore } from '../../../app/stores/store';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 
-export default function ActivityDetails() {
+export default observer(function ActivityDetails() {
 
     const {activityStore} = useStore();
-    const {selectedActivity: activity} = activityStore;
+    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams();
+    
+    useEffect(() => {
+        if (id) loadActivity(id);
+    }, [id, loadActivity])
 
-    if (!activity) return <LoadingComponent />
+    if (loadingInitial || !activity) return <LoadingComponent />;
 
     return (
         <Card fluid>
@@ -31,4 +38,4 @@ export default function ActivityDetails() {
             </Card.Content>
         </Card>
     )
-}
+})
