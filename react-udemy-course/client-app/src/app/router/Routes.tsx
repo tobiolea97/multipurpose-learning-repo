9c3063a80a59499362eba6232a3c28a5564a6c45
Tobias Olea <tobiolea97@gmail.com>
2023-11-02 00:40:58 -0300

    Adding a details link

--- react-udemy-course/client-app/src/app/router/Routes.tsx
+++ react-udemy-course/client-app/src/app/router/Routes.tsx
@@ -12,6 +12,7 @@ export const routes: RouteObject[] = [
         children: [
             {path: '', element: <HomePage />},
             {path: 'activities', element: <ActivityDashboard />},
+            {path: 'activities/:id', element: <ActivityDetails />},
             {path: 'createActivity', element: <ActivityForm />},
         ]
     }
