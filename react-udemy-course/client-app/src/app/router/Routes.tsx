d0f4d8ef31092315d276edb0b920b62318a2fa90
Tobias Olea <tobiolea97@gmail.com>
2023-11-02 00:29:40 -0300

    Adding routes

--- react-udemy-course/client-app/src/app/router/Routes.tsx
+++ react-udemy-course/client-app/src/app/router/Routes.tsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter, RouteObject } from "react-router-dom";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import HomePage from "../../features/home/HomePage";
 import App from "../layout/App";
 
 export const routes: RouteObject[] = [
     {
-        path: "/",
+        path: '/',
         element: <App />,
+        children: [
+            {path: '', element: <HomePage />},
+            {path: 'activities', element: <ActivityDashboard />},
+            {path: 'createActivity', element: <ActivityForm />},
+        ]
     }
 ]
 
