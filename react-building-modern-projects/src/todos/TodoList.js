430ab2124fcf773d666cdab8d195c7b7ce62dd5c
Tobias Olea <tobiolea97@gmail.com>
2023-11-12 16:03:44 -0300

    Putting the app together

--- react-building-modern-projects/src/todos/TodoList.js
+++ react-building-modern-projects/src/todos/TodoList.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
+import './TodoList.css';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos = [] }) => (
     <div className="list-wrapper">
+        <NewTodoForm />
         {todos.map(todo => <TodoListItem todo={todo} />)}
     </div>
 );
