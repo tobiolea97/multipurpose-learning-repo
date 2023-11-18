8f9e07e3c6a600e65e8e6fc210338fb63509f253
Tobias Olea <tobiolea97@gmail.com>
2023-11-18 19:46:43 -0300

    Deleting records

--- react-router-tutorial/router-tutorial/src/main.jsx
+++ react-router-tutorial/router-tutorial/src/main.jsx
@@ -6,6 +6,7 @@ import Root, { loader as rootLoader, action as rootAction, } from "./routes/root
 import ErrorPage from "./error-page";
 import Contact, {loader as contactLoader,} from "./routes/contact";
 import EditContact, { action as editAction} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -19,8 +20,9 @@ const router = createBrowserRouter([
         (You might note we reused the contactLoader for this route. This is only because we're being lazy in the tutorial.
         There is no reason to attempt to share loaders among routes, they usually have their own
       */
-      {path: "contacts/:contactId",       element: <Contact />,         loader: contactLoader,},
-      {path: "contacts/:contactId/edit",  element: <EditContact />,     loader: contactLoader,      action: editAction,},
+      {path: "contacts/:contactId",         element: <Contact />,         loader: contactLoader,},
+      {path: "contacts/:contactId/edit",    element: <EditContact />,     loader: contactLoader,      action: editAction,},
+      {path: "contacts/:contactId/destroy", action: destroyAction,},
     ],
   },
 ]);
