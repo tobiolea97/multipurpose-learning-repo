1844e7354e5cb3f0cb9a73ed803f4d9dc39692db
Tobias Olea <tobiolea97@gmail.com>
2023-11-18 19:40:17 -0300

    Global Pending UI. Spinner when a element is chosen.

--- react-router-tutorial/router-tutorial/src/routes/root.jsx
+++ react-router-tutorial/router-tutorial/src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form, redirect, NavLink, } from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink, useNavigation,} from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -13,6 +13,7 @@ export async function loader() {
 
 export default function Root() {
     const { contacts } = useLoaderData();
+    const navigation = useNavigation();
     return (
       <>
         <div id="sidebar">
@@ -74,7 +75,11 @@ export default function Root() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div id="detail"
+            className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+          >
             <Outlet />
         </div>
       </>
