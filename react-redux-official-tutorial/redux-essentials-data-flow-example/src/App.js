2bbef948e9721b81dd679668c8a816268d956881
Tobias Olea <tobiolea97@gmail.com>
2023-11-29 22:52:34 -0300

    Adding a posts list

--- react-redux-official-tutorial/redux-essentials-data-flow-example/src/App.js
+++ react-redux-official-tutorial/redux-essentials-data-flow-example/src/App.js
@@ -3,10 +3,12 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
+  Redirect
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
+
+import { PostsList } from './features/posts/PostsList'
 
 function App() {
   return (
@@ -18,9 +20,9 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <React.Fragment>
+                <PostsList />
+              </React.Fragment>
             )}
           />
           <Redirect to="/" />
