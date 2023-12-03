d56d0ccf29063604f9b3c8bad9ff67e800fdb186
Tobias Olea <tobiolea97@gmail.com>
2023-12-03 00:28:07 -0300

    Post reaction buttons

--- react-redux-official-tutorial/redux-essentials-data-flow-example/src/features/posts/PostsList.js
+++ react-redux-official-tutorial/redux-essentials-data-flow-example/src/features/posts/PostsList.js
@@ -3,10 +3,11 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
 export const PostsList = () => {
   const posts = useSelector(state => state.posts)
-  
+
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
   
   const renderedPosts = orderedPosts.map(post => {
@@ -21,6 +22,7 @@ export const PostsList = () => {
         <Link to={`/posts/${post.id}`} className="button muted-button">
           View Post
         </Link>
+        <ReactionButtons post={post} />
       </article>
     )
   })
