6751a018acf7797b8dd5e0a4b2e7778042cfec3a
Tobias Olea <tobiolea97@gmail.com>
2023-12-03 15:43:34 -0300

    Converting users and notifications slice

--- react-redux-official-tutorial/redux-essentials-data-flow-example/src/features/posts/AddPostForm.js
+++ react-redux-official-tutorial/redux-essentials-data-flow-example/src/features/posts/AddPostForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { addNewPost } from './postsSlice'
+import { selectAllUsers } from '../users/usersSlice' 
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
@@ -11,7 +12,7 @@ export const AddPostForm = () => {
 
   const dispatch = useDispatch()
 
-  const users = useSelector(state => state.users)
+  const users = useSelector(selectAllUsers)
 
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
--- react-redux-official-tutorial/redux-essentials-data-flow-example/src/features/users/usersSlice.js
+++ react-redux-official-tutorial/redux-essentials-data-flow-example/src/features/users/usersSlice.js
@@ -1,27 +1,25 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
+
+const usersAdapter = createEntityAdapter()
+
+const initialState = usersAdapter.getInitialState()
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
   const response = await client.get('/fakeApi/users')
   return response.data
 })
 
-const initialState = []
-
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      return action.payload
-    })
+    builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
   }
 })
 
-export const selectAllUsers = state => state.users
-
-export const selectUserById = (state, userId) =>
-  state.users.find(user => user.id === userId)
-
 export default usersSlice.reducer
+
+export const { selectAll: selectAllUsers, selectById: selectUserById } =
+  usersAdapter.getSelectors(state => state.users)
