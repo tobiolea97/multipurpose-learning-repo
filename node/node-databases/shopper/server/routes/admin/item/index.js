5804e30e7f7331688fef4728c3d2005e9ff19bd0
Tobias Olea <tobiolea97@gmail.com>
2024-02-01 23:51:33 -0300

    Implementing the database methods

--- node/node-databases/shopper/server/routes/admin/item/index.js
+++ node/node-databases/shopper/server/routes/admin/item/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
+const ItemService = require("../../../services/ItemService");
+
 module.exports = () => {
   const router = express.Router();
 
-  router.get("/:itemId?", async (req, res) => {
-    return res.render("admin/item", {});
-
-    /*
+  router.get("/:itemId?", async (req, res, next) => {
     try {
       const items = await ItemService.getAll();
       let item = null;
@@ -23,14 +22,10 @@ module.exports = () => {
     } catch (err) {
       return next(err);
     }
-    */
   });
 
   // Save or update item
-  router.post("/", async (req, res, next) => {
-    return next("Not implemented");
-
-    /*
+  router.post("/", async (req, res) => {
     // Massage the passed in form data a bit
     const sku = req.body.sku.trim();
     const name = req.body.name.trim();
@@ -72,16 +67,12 @@ module.exports = () => {
       console.error(err);
       return res.redirect("/admin/item");
     }
-    */
   });
 
   // Delete item
-  router.get("/delete/:itemId", async (req, res, next) => {
-    return next("Not implemented");
-
-    /*
+  router.get("/delete/:itemId", async (req, res) => {
     try {
-      await ItemService.remove(req.params.itemId);
+      await ItemService.remove({ _id: req.params.itemId });
     } catch (err) {
       // Error handling
       req.session.messages.push({
@@ -97,7 +88,6 @@ module.exports = () => {
       text: "The item was successfully deleted!",
     });
     return res.redirect("/admin/item");
-    */
   });
   return router;
 };
