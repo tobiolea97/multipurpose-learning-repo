489c890cfd97a2decf47b05c47b0cfa314c1e2c4
Tobias Olea <tobiolea97@gmail.com>
2024-02-04 16:10:13 -0300

    Route chaining

--- node/node-express/project/index.js
+++ node/node-express/project/index.js
@@ -25,6 +25,31 @@ app.get('/redirect', (request, response) => {
     response.redirect('http://www.linkedin.com')
 })
 
+//Route chaining
+app.route('/class')
+.get((request, response) => {
+    response.send('Retrieve class info')
+}).post((request, response) => {
+    response.send('Create class info')
+}).put((request, response) => {
+    response.send('Update class info')
+})
+
+//GET
+// app.get('/class', (request, response) => {
+//     response.send('Retrieve class info')
+// });
+
+//POST
+// app.post('/class', (request, response) => {
+//     response.send('Create class info')
+// });
+
+//PUT
+// app.put('/class', (request, response) => {
+//     response.send('Update class info')
+// })
+
 //GET with next()
 app.get('/next', (request, response, next) => {
    console.log("The response will be sent by the next function.")
