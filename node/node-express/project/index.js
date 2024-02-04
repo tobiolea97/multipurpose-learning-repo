b4d4c0de9e479fc18b9c4e3fb41c3e9ae5a902e1
Tobias Olea <tobiolea97@gmail.com>
2024-02-04 15:48:18 -0300

    Using route handlers for adding more than one callback to a request

--- node/node-express/project/index.js
+++ node/node-express/project/index.js
@@ -15,6 +15,14 @@ app.get('/', (request, response) => {
     response.json(data);
 })
 
+//GET with next()
+app.get('/next', (request, response, next) => {
+   console.log("The response will be sent by the next function.")
+   next()
+}, (request, response) =>{
+    response.send('I just set up a route with a second callback.')
+})
+
 //GET with Routing Parameters
 app.get('/class/:id', (request, response)=> {
     const studentId = Number(request.params.id);
