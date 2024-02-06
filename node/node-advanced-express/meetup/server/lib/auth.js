97514ea6030ce946209118f67462ea94690d192c
Tobias Olea <tobiolea97@gmail.com>
2024-02-06 08:14:24 -0300

    Serializing and deserializing users

--- node/node-advanced-express/meetup/server/lib/auth.js
+++ node/node-advanced-express/meetup/server/lib/auth.js
@@ -18,6 +18,18 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, async (username, pass
   }
 }));
 
+// eslint-disable-next-line no-underscore-dangle
+passport.serializeUser((user, done) => done(null, user._id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await UserModel.findById(id).exec();
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
+
 module.exports = {
   initialize: passport.initialize(),
   session: passport.session(),
