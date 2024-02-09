57686673c1490b86790c2c62270cc026ae7accc4
Tobias Olea <tobiolea97@gmail.com>
2024-02-09 14:34:23 -0300

    Protecting routes

--- node/node-advanced-express/meetup/server/routes/users/index.js
+++ node/node-advanced-express/meetup/server/routes/users/index.js
@@ -4,19 +4,24 @@ const UserModel = require('../../models/UserModel');
 
 const router = express.Router();
 
+function redirectIfLoggedIn(req, res, next) {
+  if (req.user) return res.redirect('/users/account');
+  return next();
+}
+
 module.exports = () => {
   router.post('/login', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/users/login?error=true',
   }));
-  router.get('/login', (req, res) => res.render('users/login', { error: req.query.error }));
+  router.get('/login', redirectIfLoggedIn, (req, res) => res.render('users/login', { error: req.query.error }));
 
   router.get('/logout', (req, res) => {
     req.logout();
     return res.redirect('/');
   });
 
-  router.get('/registration', (req, res) => res.render('users/registration', { success: req.query.success }));
+  router.get('/registration', redirectIfLoggedIn, (req, res) => res.render('users/registration', { success: req.query.success }));
   router.post('/registration', async (req, res, next) => {
     try {
       const user = new UserModel({
@@ -33,7 +38,10 @@ module.exports = () => {
     }
   });
 
-  router.get('/account', (req, res) => res.render('users/account', { user: req.user }));
+  router.get('/account', (req, res, next) => {
+    if (req.user) return next();
+    return res.status(401).end();
+  }, (req, res) => res.render('users/account', { user: req.user }));
 
   return router;
 };
