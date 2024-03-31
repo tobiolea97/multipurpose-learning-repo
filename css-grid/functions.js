b31585a27a94db5f4d22261bfc8e7f56e786c3e2
Tobias Olea <tobiolea97@gmail.com>
2024-03-31 17:01:30 -0300

    Animating the sliding effect.

--- css-grid/functions.js
+++ css-grid/functions.js
@@ -8,6 +8,7 @@ let screenReaderText = document.querySelector('.trigger .screen-reader-text');
 // Toggle reveal class on body element, set aria-expanded and screen reader text on TRIGGER:
 function revealMenu() {
     SITE.classList.toggle('reveal');
+	REVEAL.classList.add('open');
     TRIGGER.getAttribute('aria-expanded') == 'false' ? TRIGGER.setAttribute('aria-expanded', true) : TRIGGER.setAttribute('aria-expanded', false);
     screenReaderText.innerHTML == 'Reveal menu' ? screenReaderText.innerHTML = 'Hide menu' : screenReaderText.innerHTML = 'Reveal menu';
 }
@@ -21,13 +22,18 @@ function catchFocus() {
 	}
 }
 
+function removeMenu() {
+	if ( TRIGGER.getAttribute('aria-expanded') == 'false') {
+		REVEAL.classList.remove('open');
+	}
+}
+
 // Hide nav area when touch or click happens elsewhere:
 function clickTarget(e) {
 	if ( TRIGGER.getAttribute('aria-expanded') == 'true' && !REVEAL.contains(e.target) ) {
 		revealMenu();
 	}
 }
-
 
 // Liten for clicks on TRIGGER button:
 TRIGGER.addEventListener('click', revealMenu, false);
@@ -37,3 +43,5 @@ SITE.addEventListener('focusin', catchFocus, true);
 
 // Listen for clicks:
 SITE.addEventListener('click', function(e) { clickTarget(e); }, true);
+
+SITE.addEventListener('transitionend', removeMenu, false);
