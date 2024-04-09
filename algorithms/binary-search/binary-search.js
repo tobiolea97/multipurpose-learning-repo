9d97845bbb3092e9158a86f9eb01fd6e629dd82a
Tobias Olea <tobiolea97@gmail.com>
2024-04-09 19:22:26 -0300

    Binary search

--- algorithms/binary-search/binary-search.js
+++ algorithms/binary-search/binary-search.js
@@ -1,5 +1,4 @@
 function iterativeBinarySearch(array, number) { 
-    debugger;
 	let left = 0;
 	let right = array.length - 1;
 	let middle;
@@ -28,23 +27,23 @@ function iterativeBinarySearch(array, number) {
 }
 
 
-function recursiveBinarySearch(arr, l, r, x){
-	if (r >= l) {
-		let mid = l + Math.floor((r - l) / 2);
+function recursiveBinarySearch(array, left, right, x){
+	if (right >= left) {
+		let mid = left + Math.floor((right - left) / 2);
 
 		// If the element is present at the middle
 		// itself
-		if (arr[mid] == x)
+		if (array[mid] == x)
 			return mid;
 
 		// If element is smaller than mid, then
 		// it can only be present in left subarray
-		if (arr[mid] > x)
-			return recursiveBinarySearch(arr, l, mid - 1, x);
+		if (array[mid] > x)
+			return recursiveBinarySearch(array, left, mid - 1, x);
 
 		// Else the element can only be present
 		// in right subarray
-		return recursiveBinarySearch(arr, mid + 1, r, x);
+		return recursiveBinarySearch(array, mid + 1, right, x);
 	}
 
 	// We reach here when element is not
@@ -52,11 +51,11 @@ function recursiveBinarySearch(arr, l, r, x){
 	return -1;
 }
 
-let arr = [ 1, 2, 3, 4, 10, 40 ];
+let array = [ 1, 2, 3];
 let x = 10;
-let n = arr.length
-let resultIterative = iterativeBinarySearch(arr, x);
-let resultBinary = recursiveBinarySearch(arr, 0, n - 1, x);
+let n = array.length
+let resultIterative = iterativeBinarySearch(array, x);
+let resultBinary = recursiveBinarySearch(array, 0, n - 1, x);
 
 console.log("Iterative Binary Search");
 (resultIterative == -1) ? console.log( "Element is not present in array") : console.log("Element is present at index " +resultIterative);
