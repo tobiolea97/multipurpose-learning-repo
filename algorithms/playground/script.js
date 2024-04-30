b75606ce289913a308f06aa84b8ad4ba78f9ca83
Tobias Olea <tobiolea97@gmail.com>
2024-04-30 08:27:49 -0300

    Adding insertion sort to playground

--- algorithms/playground/script.js
+++ algorithms/playground/script.js
@@ -2,10 +2,9 @@
 *   Fibo
 *****************************************/
 const fibo = (number) => {
-    return 0;
 }
 let arrayFibo = []
-for(let i = 0; i < 10; i++) {
+for(let i = 0; i < 15; i++) {
     arrayFibo.push(fibo(i));
 }
 // console.log(arrayFibo);
@@ -14,18 +13,25 @@ for(let i = 0; i < 10; i++) {
 *   Selection sort
 *****************************************/
 const selectionSort = (array) => {
-    return array;
 } 
-// console.log(selectionSort([3,5,8,9,6,4,1,2,7,0,100,-100,14]));
+// console.log(selectionSort([3,5,8,9,6,4,1,2,7,0,100,-100,14,2]));
 
 
 /*****************************************
-*   Selection sort
+*   Bubble sort
 *****************************************/
 const bubbleSort = (array) => {
-    return array;
+    
 } 
 // console.log(bubbleSort([3,5,8,9,6,4,1,2,7,0,100,-100,14]));
+
+/*****************************************
+*   Bubble sort
+*****************************************/
+const insertionSort = (array) => {
+    
+} 
+// console.log(insertionSort([3,5,8,9,6,4,1,2,7,0,100,-100,14]));
 
 
 /*********************************************************************/
@@ -35,13 +41,12 @@ let sortedArray = [1,2,3,4,5,6,7,8,9,10,11,12];
 *   Recursive binary search
 *****************************************/
 const recursiveBinarySearch = () => {
-    
 }
-//console.log(recursiveBinarySearch());
+// console.log(recursiveBinarySearch());
 
 /*****************************************
 *   Linear binary search
 *****************************************/
 const linearBinarySearch = () => {
 }
-console.log();
+// console.log(linearBinarySearch());
