c8ccab0db42c2917890c90e3028168b54f3e26d7
Tobias Olea <tobiolea97@gmail.com>
2024-05-04 08:26:31 -0300

    Compacting insertion algorithm. Correcting playground.

--- algorithms/sorting/insertion/script.js
+++ algorithms/sorting/insertion/script.js
@@ -1,12 +1,10 @@
-function insertionSort(arr)  
-{
+function insertionSort(arr) {
     let length = arr.length;
-    let i, key, j;  
-    for (i = 1; i < length; i++) 
+    for (let i = 1; i < length; i++) 
     {  
-        key = arr[i];  
-        j = i - 1;
-        while (j >= 0 && arr[j] > key) 
+        let key = arr[i];  
+        let j = i - 1;
+        while ( j >= 0 && arr[j] > key) 
         {  
             arr[j + 1] = arr[j];  
             j = j - 1;  
