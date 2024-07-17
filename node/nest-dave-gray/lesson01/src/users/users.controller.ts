c42b3259b967526e024ee204b19633cc54bc08e5
Tobias Olea <tobiolea97@gmail.com>
2024-07-16 23:41:52 -0300

    Full Nestjs course content

--- node/nest-dave-gray/lesson01/src/users/users.controller.ts
+++ node/nest-dave-gray/lesson01/src/users/users.controller.ts
@@ -1,34 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {
-  @Get()
-  findAll(@Query('role') role ?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
-  }
-
-  @Get('interns')
-  findAllInterns() {
-    return [];
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return { id };
-  }
-
-  @Post()
-  create(@Body() user: {}) {
-    return user;
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() user: {}) {
-    return user;
-  }
-
-  @Delete(':id')
-  delete(@Param('id') id: string) {
-    return { id };
-  }
-}
+export class UsersController {}
